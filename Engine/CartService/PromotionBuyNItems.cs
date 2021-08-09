929f3031c36de18c4c32a12889ac7c969b44a627
Nitish Kakkar <nitishkakkar@outlook.com>
2021-08-09 14:09:17 +0200

    Adding other promotion

---
+++ Engine/CartService/PromotionBuyNItems.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataModel;
+
+namespace CartService
+{
+    class PromotionBuyNItems : IPromotion
+    {
+        public string ItemId { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+
+        public PromotionBuyNItems(string itemId, int itemCount, double price)
+        {
+            ItemId = itemId;
+            Count = itemCount;
+            Price = price;
+        }
+        public double ApplyPromotion(List<CartItem> cartItems)
+        {
+            var cartItem = cartItems.Where(s => s.Item.ProductId == ItemId).FirstOrDefault();
+
+            if (cartItem != null)
+            {
+                cartItems.Remove(cartItem);
+                return (cartItem.Quantity / Count) * Price + (cartItem.Quantity % Count) * cartItem.Item.Price;
+            }
+
+            return 0;
+        }
+    }
+}
