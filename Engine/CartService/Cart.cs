8d94777e4d91215b1892482c58de1ba4fba726d8
Nitish Kakkar <nitishkakkar@outlook.com>
2021-08-09 14:13:41 +0200

    Adding Cart which will calculate the price and apply promotion

---
+++ Engine/CartService/Cart.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Collections.Generic;
+using DataModel;
+
+namespace CartService
+{
+    class Cart
+    {
+        public decimal CartPrice { get; set; }
+
+        public List<CartItem> cartItems;
+        private List<IPromotion> promotions;
+
+        public Cart()
+        {
+            cartItems = new List<CartItem>();
+            promotions = new List<IPromotion>();
+        }
+
+        public void AddCartItem(CartItem cartItem)
+        {
+            cartItems.Add(cartItem);
+        }
+
+        public void AddPromotion(IPromotion promotion)
+        {
+            promotions.Add(promotion);
+        }
+
+        public double ApplyPromotion()
+        {
+            double CartPrice = 0;
+            List<CartItem> cartItemsInCart = new List<CartItem>(cartItems);
+            foreach (var promotion in promotions)
+            {
+                CartPrice += promotion.ApplyPromotion(cartItemsInCart);
+            }
+
+            return CartPrice + GetCartPrice(cartItemsInCart);
+        }
+
+        private double GetCartPrice(List<CartItem> cartItems)
+        {
+            double price = 0;
+            foreach (var cartItem in cartItems)
+            {
+                price += cartItem.Item.Price * cartItem.Quantity;
+            }
+
+            return price;
+        }
+
+        public double GetPriceofAllCartItems()
+        {
+            return GetCartPrice(cartItems);
+        }
+    }
+}
